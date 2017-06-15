033947b732d43d7b7b64ba253fc2742c5c581068
Alex Qin <qianjin.qin@qq.com>
2017-06-15 08:17:29 -0700

    code: add logout

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Grid, Row, Col, Button, Input, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 export default class Main extends React.Component{
+    handleLogOut(){
+        sessionStorage.removeItem('login');
+        this.props.history.push('/');
+    }
+
     render(){
         return (
-            <h3>This is the main page!</h3>
+            <div>
+                <h3>This is the main page!</h3>
+                <Button onClick={this.handleLogOut.bind(this)}>Log out</Button>
+            </div>
         );
     }
 }
