9e957626800676429acfbdb1e32f7372bc0d9e9b
Alex Qin <qianjin.qin@qq.com>
2017-06-17 04:53:53 -0700

    code: update api url to heroku web api

--- src/actions/login.js
+++ src/actions/login.js
@@ -18,8 +18,10 @@ export const login = (userData, loginCallback) => {
         body: JSON.stringify(body)
     }
     //http://reactjsblueprintsuseradmin.herokuapp.com/v1/login
+    // `http://localhost:5000/v1/login`
+    const apiUrl = 'https://weaponapi.herokuapp.com/v1/login';
     return dispatch => {
-        return fetch(`http://localhost:5000/v1/login`, options)
+        return fetch(apiUrl, options)
             .then(response => response.json())
             .then(json => dispatch(setLoginDetails(json, loginCallback)))
     }
