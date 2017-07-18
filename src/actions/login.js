876c607a812cd2220205a91b9d0e29d913d4dfb6
Alex Qin <qianjin.qin@qq.com>
2017-07-18 08:38:15 -0700

    code: update to remote api url

--- src/actions/login.js
+++ src/actions/login.js
@@ -17,8 +17,8 @@ export const login = (userData, loginCallback) => {
         body: JSON.stringify(body)
     };
     
-    const apiUrl = `http://localhost:5000/v1/token`;
-    // const apiUrl = 'https://weaponapi.herokuapp.com/v1/login';
+    // const apiUrl = `http://localhost:5000/v1/token`;
+    const apiUrl = 'https://weaponapi.herokuapp.com/v1/login';
     return dispatch => {
         return fetch(apiUrl, options)
             .then(response => response.json())
