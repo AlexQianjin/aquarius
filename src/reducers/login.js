0440f213abc171ecae16feaf8368d9088acf2758
Alex Qin <qianjin.qin@qq.com>
2017-07-20 15:14:18 +0800

    code: update actions

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,18 +1,18 @@
 'use strict';
-import { LOGIN_USER, LOGIN_FAIL} from '../constants/ActionTypes';
+import { REQUEST_LOGIN_USER, RECEIVE_LOGIN_USER, RECEIVE_LOGIN_FAIL} from '../constants/ActionTypes';
 
 export default function user(state = {
     message: "",
     userData: {}
 }, action) {
     switch (action.type) {
-        case LOGIN_USER:
+        case RECEIVE_LOGIN_USER:
             return {
                 ...state,
                 userData: action.loginResponse[0],
                 timestamp: action.timestamp
             };
-        case LOGIN_FAIL:
+        case RECEIVE_LOGIN_FAIL:
             return {
                 ...state,
                 userData: [],
