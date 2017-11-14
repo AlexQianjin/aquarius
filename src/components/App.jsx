8401a1015cf8a9f3edcfa05e8ae74b49e4250ce0
Alex Qin <qianjin.qin@qq.com>
2017-11-14 07:00:37 -0800

    add files

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import store from '../stores/store';
+import Routes from './Router';
+
+import {Provider} from 'react-redux';
+
+class App extends React.Component {
+	render() {
+		return (
+			<Provider store={store}>
+				{Routes}
+			</Provider>
+		);
+	}
+}
+
+export default App;
