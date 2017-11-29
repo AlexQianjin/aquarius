e94dd373695dc57f60c5f18bf8ac5bb16000bd34
Alex Qin <qianjin.qin@qq.com>
2017-11-29 17:12:35 +0800

    remove localhost

--- server-development.js
+++ server-development.js
@@ -24,7 +24,7 @@ app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, 'localhost', function(err) {
+app.listen(port, function(err) {
 	if (err) {
 		console.log(err);
 		return;
